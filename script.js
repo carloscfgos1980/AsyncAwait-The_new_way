6b3b12931021d0ffd82950f46dcf0dab17e8c87c
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2022-06-16 07:46:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+
 async function getData() {
-    let apiUrl = 'https://api.themoviedb.org/3/genre/movie/list';
     try {
-        const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=cdb6e4fe4726f8b1dffbdff2eb676e52').then(res => res.json());
+        const res = await fetch(API_URL + API_KEY).then(res => res.json());
         console.log(res)
     } catch (err) {
         console.log(err)
